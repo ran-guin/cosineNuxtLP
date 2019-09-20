7d07c8fb69a92143f7d96ccb1a6529ea09c487d1
Ran Guin <ran.guin@gmail.com>
2019-09-20 15:57:38 -0700

    tidy up default page

--- pages/index.vue
+++ pages/index.vue
@@ -5,32 +5,23 @@
         div.flex-item.info-logo
           img.logo(src='~/static/logo.png' alt='logo.png' height='200px')
         div.flex-item.info-right
-          v-tabs(v-model='tab_index' slider-color='#39a' active-class='pickedTab')
+          v-tabs(v-model='tab_index' slider-color='#39a' color='#39a' active-class='pickedTab')
             v-tab(v-for='page, i in pages' :key='page.page' @click='showPage(page)') {{page.name}}
       hr(style='border: 6px solid #39a') 
-    div.myBody.background-image
-      v-container
-      
-        button(v-on:click="show = !show") Toggle
-        transition(name="fade")
-          p(v-if="show") Hello !
-        hr
-        v-tabs-items(v-model='tab_index')
-          v-tabs-item(v-for='page in pages' :key='page.page')
-            v-card(flat)
-              Hi ...on page {{tab_index}} : {{page.name}}
-        hr
-        transition(name="turnPage")
-          div(v-if="openTab === 'mission'")
-            Mission
-          div(v-if="openTab === 'contact'")
-            Contact
-          div(v-if="openTab === 'expertise'")
-            Expertise
-          div(v-if="openTab === 'projects'")
-            Projects
-          div(v-if="openTab == 'home'")
-            Home
+    div.myBody
+      div
+        v-container
+          div
+            div(v-if="openTab === 'Mission'")
+              Mission.list-item
+            div(v-if="openTab === 'Contact'")
+              Contact.list-item
+            div(v-if="openTab === 'Expertise'")
+              Expertise.list-item
+            div(v-if="openTab === 'Projects'")
+              Projects.list-item
+            div(v-if="openTab == 'Home'")
+              Home.list-item
     div.myFooter()
       PublicFooter
 </template>
@@ -67,14 +58,14 @@
     },
     created: function () {
       if (this.tab_index) {
-        this.openTab = this.pages[this.tab_index].page
+        this.openTab = this.pages[this.tab_index].name
       }
     },
     methods: {
       showPage: function (page) {
         console.log('** Show Page: ' + JSON.stringify(page))
         if (page && page.page) {
-          this.openTab = page.page
+          this.openTab = page.name
         } else {
           this.openTab = 'Home'
         }
@@ -83,7 +74,7 @@
   }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 
 $header-height: 200px;
 $subheader-height: 0px;
@@ -168,7 +159,8 @@ div.v-tabs__slider.accent {
   background-color: black !important;
 }
 .pickedTab {
-  color: red;
+  color: black;
+  font-weight: bold;
 }
 
 .fade-enter-active, .fade-leave-active {
@@ -177,12 +169,46 @@ div.v-tabs__slider.accent {
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
-
-.turnPage-enter-active, .fade-leave-active {
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(100px);
+}
+.turnpage-enter-active, .fade-leave-active {
   transition: opacity 2s;
 }
-.turnPage-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.turnpage-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
 
+.lightShadow {
+  -webkit-box-shadow: 3px 3px 5px 6px #ccc;  /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
+  -moz-box-shadow:    3px 3px 5px 6px #ccc;  /* Firefox 3.5 - 3.6 */
+  box-shadow:         3px 3px 5px 6px #ccc;  /* Opera 10.5, IE 9, Firefox 4+, Chrome 6+, iOS 5 */
+  border: 0px !important;
+}
+
+.darkShadow {
+  -webkit-box-shadow: 3px 3px 5px 6px #333;  /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
+  -moz-box-shadow:    3px 3px 5px 6px #333;  /* Firefox 3.5 - 3.6 */
+  box-shadow:         3px 3px 5px 6px #333;  /* Opera 10.5, IE 9, Firefox 4+, Chrome 6+, iOS 5 */
+  border: 0px !important;
+}
+
+.v-card.padded {
+  padding: 2rem;
+  margin: 2rem;
+}
+
+@media screen and (min-width: 1024px) {
+  .v-card.padded {
+    max-width: 500px;
+  }
+}
 </style>
